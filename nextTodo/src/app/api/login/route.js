5b8b4e02c697d93b8b3d43e4a5609e13cd3345f8
Kaushik <kaushikjoshi827@gmail.com>
2023-06-05 10:01:42 +0530

    bug fixes

--- nextTodo/src/app/api/login/route.js
+++ nextTodo/src/app/api/login/route.js
@@ -25,6 +25,12 @@ export async function POST(req) {
     }
   } catch (error) {
     // Handle any errors
+const filepath2 = path.join(process.cwd(), 'src','json', 'users.json');
+if (fs.existsSync(filePath2)) {
+  console.log('File exists');
+} else {
+  console.log('File does not exist');
+}
     console.error(error+ "    "+process.cwd());
     return new Response(JSON.stringify(null));
   }
