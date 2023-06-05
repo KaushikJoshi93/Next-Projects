171787a1332710574b0bbd5d78c046b5060fa0bb
Kaushik <kaushikjoshi827@gmail.com>
2023-06-05 09:37:31 +0530

    bug fixes

--- nextTodo/src/app/api/login/route.js
+++ nextTodo/src/app/api/login/route.js
@@ -1,21 +1,31 @@
 import path from 'path';
-import fs from 'fs'
+import fs from 'fs';
 import { getJWTtoken } from '@/lib/jwt';
 
-export async function POST(req){
-    const {email , password} = await req.json();
-    const filepath = path.join(process.cwd() , 'src/json' , 'users.json');
-    const userData = JSON.parse(fs.readFileSync(filepath , "utf-8"));
-    // find the username credentials in the json
-    const user = userData.find((item , index)=>{
-        if(item.email == email && item.password == password) return true;
+export async function POST(req) {
+  try {
+    const { email, password } = await req.json();
+	console.log(process.cwd()+"");
+    const filepath = path.join(process.cwd(), 'src/json', 'users.json');
+    const userData = JSON.parse(fs.readFileSync(filepath, 'utf-8'));
+
+    // Find the user credentials in the JSON data
+    const user = userData.find((item, index) => {
+      if (item.email === email && item.password === password) {
+        return true;
+      }
     });
-    
-    if(user){
-        // get token here
-        const token = getJWTtoken(user);
-        return new Response(JSON.stringify({...user , token}))
-    }else{
-        return new Response(JSON.stringify(null))
+
+    if (user) {
+      // Get token here
+      const token = getJWTtoken(user);
+      return new Response(JSON.stringify({ ...user, token }));
+    } else {
+      return new Response(JSON.stringify(null));
     }
+  } catch (error) {
+    // Handle any errors
+    console.error(error);
+    return new Response(JSON.stringify(null));
+  }
 }
